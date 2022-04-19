753530ba6342958efc7c6f8c2a79f17a5ab69572
hnqle3 <hoa.nq.le3@gmail.com>
2022-04-19 00:05:28 -0400

    created three routes

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const mongoose = require("mongoose");
 
+
+const users = require("./routes/api/users");
+const profiles = require("./routes/api/profiles");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 // Allow you to perform cross-domain Ajax requests in web applications.
@@ -21,8 +26,12 @@ mongoose
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("hello!"));
+    app.get("/", (req, res) => res.send("hello!"));
 
+    // Use routes
+    app.use("/api/users", users);
+    app.use("/api/profiles", profiles);
+    app.use("/api/posts", posts);
 // The actual port for the app in prodcution will come from the env. var. "PORT". For local dev., we just use 8080.
 const port = process.env.PORT || 8080;
 
