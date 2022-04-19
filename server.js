092d3943d0c41141c59365deb9d766c305c97e7d
hnqle3 <hoa.nq.le3@gmail.com>
2022-04-19 00:36:43 -0400

    passport jwt authencticaiton strategy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const users = require("./routes/api/users");
 const profiles = require("./routes/api/profiles");
 const posts = require("./routes/api/posts");
-
+const passport = require("passport");
 const app = express();
 
 // Allow you to perform cross-domain Ajax requests in web applications.
@@ -28,7 +28,13 @@ mongoose
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
 
-    app.get("/", (req, res) => res.send("hello!"));
+    // app.get("/", (req, res) => res.send("hello!"));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
 
     // Use routes
     app.use("/api/users", users);
